5f1626e965c5f5bad3e08b623f59e8ce541a0236
ZakariaBj <zakariabenjeddi22@gmail.com>
2024-11-10 16:27:31 +0100

    base donne

---
+++ etudiant.sql
@@ -0,0 +1,9 @@
+-- Active: 1713815680743@@localhost@3306
+CREATE DATABASE ecole ;
+USE ecole ;
+CREATE TABLE Etudiant (
+  id_etudiant int(10) PRIMARY KEY AUTO INCREMENT ,
+  nom varchar(20) DEFAULT "zakaria" NOT NULL,
+  prenom varchar(20) DEFAULT "ben jeddi" NOT NULL,
+  age int(20) DEFAULT 0 NOT NULL,
+);
